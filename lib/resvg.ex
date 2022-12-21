a54893e3cd4203c4ebb368057b8cffe45a28d9aa
Tymon Tobolski <teamon@me.com>
2022-12-21 19:28:23 +0100

    init

---
+++ lib/resvg.ex
@@ -0,0 +1,18 @@
+defmodule Resvg do
+  @moduledoc """
+  Documentation for `Resvg`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Resvg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/resvg_test.exs
@@ -0,0 +1,8 @@
+defmodule ResvgTest do
+  use ExUnit.Case
+  doctest Resvg
+
+  test "greets the world" do
+    assert Resvg.hello() == :world
+  end
+end
